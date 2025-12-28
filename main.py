1af9dbf7dd19b93a1c22408af3b36442a42a258b
camoceltic <camoceltic@yahoo.com>
2025-12-28 15:53:40 -0800

    Connects and correctly sends the (hardcoded) prompt.

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ if api_key == None:
 client = genai.Client(api_key=api_key)
 
 def main():
-    print(api_key)    
+    model = "gemini-2.5-flash"
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    content_response = client.models.generate_content(model = model, contents = contents)
+    content_meta = content_response.usage_metadata
+    
+    if type(content_meta) == None:
+        raise RuntimeError("No usage metadata.  Likely API failure.")
 
+    print(f"Prompt tokens: {content_meta.prompt_token_count}\nResponse tokens: {content_meta.candidates_token_count}")
+    print(content_response.text)
+    return 0
 
 if __name__ == "__main__":
     main()
